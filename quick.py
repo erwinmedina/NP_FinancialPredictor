730794389cf477d5ac8c997e637e767e40a90ec8
Medina-Leguizamo <erwinmedina@csu.fullerton.edu>
2024-04-15 12:41:35 -0700

    added image path

--- quick.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# Open the JSON file containing the EIN values
-with open('eins.json', 'r') as file:
-    ein_list = json.load(file)
-
-# Print the length of the list
-print("Number of EIN values:", len(ein_list))
